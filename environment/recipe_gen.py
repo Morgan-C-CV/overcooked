92d62a26602b562f58b015ee660328d9e287d89d
Morgan-C-CV <1614665906@qq.com>
2025-03-28 11:52:12 +0200

    receipe generate

--- environment/recipe_gen.py
+++ environment/recipe_gen.py
@@ -5,3 +5,7 @@ def generate_recipe(menu):
         return None
     else:
         return random.randint(0, 7)
+
+if __name__ == '__main__':
+    for i in range(30):
+        print(generate_recipe([0, 1, 2]))
